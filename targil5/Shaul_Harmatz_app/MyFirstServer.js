e1c505d06ddb1d2db6ae467ac99806860f0e940d
shaulha <shaulha@post.bgu.ac.il>
2021-09-12 22:32:51 +0300

    add5

---
+++ targil5/Shaul_Harmatz_app/MyFirstServer.js
@@ -0,0 +1,24 @@
+
+
+const express = require('express');
+const app = express();
+const port =  8080;
+var fs = require('fs');
+
+
+
+app.get( '/',(req ,res) =>{
+    res.send('Hello World!');
+    var myData = fs.readFile('gitignoreTemp.txt',function(err,data){
+        res.writeHead(200 , {'Content-Type':'text/html'});
+        res.write (data);
+        return res.end();
+    });
+    fs.appendFile('.gitignore.text',myData, function (err){
+        if (err) throw err;
+        console.log('Saved!');
+        });
+    
+});
+app.listen( port, () => {console.log('OK')} );
+
